f5b1bf6227eb65dea788f1a9b6df0999b52ae85e
Artem H <139376262+slonic32@users.noreply.github.com>
2023-12-02 20:03:29 +0200

    task 1-2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,47 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const galleryList = document.querySelector("ul.gallery");
+createGallery();
 
-console.log(galleryItems);
+galleryList.addEventListener("click", viewImage);
+
+function createGallery() {
+  const images = [];
+  for (let i = 0; i < galleryItems.length; i++) {
+    const image = `<li class="gallery__item">
+        <a class="gallery__link" href="${galleryItems[i].original}">
+            <img
+                class="gallery__image"
+                src="${galleryItems[i].preview}"
+                data-source="${galleryItems[i].original}"
+                alt="${galleryItems[i].description}"
+            />
+        </a>
+    </li>`;
+    images.push(image);
+  }
+  galleryList.innerHTML = images.join("");
+}
+
+function viewImage(event) {
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+  event.preventDefault();
+  const imageLightBox = basicLightbox.create(
+    `<img src="${event.target.dataset.source}" alt="${event.target.getAttribute(
+      "alt"
+    )}" width="800" height="600"/>`
+  );
+  imageLightBox.show(cbLightBox);
+}
+
+function cbLightBox(instance) {
+  function closeLightBox(event) {
+    if (event.code === "Escape") {
+      document.removeEventListener("keydown", closeLightBox);
+      instance.close();
+    }
+  }
+  document.addEventListener("keydown", closeLightBox);
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,25 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const galleryList = document.querySelector("ul.gallery");
+createGallery();
+let gallery = new SimpleLightbox("a.gallery__link", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
 
-console.log(galleryItems);
+function createGallery() {
+  const images = [];
+  for (let i = 0; i < galleryItems.length; i++) {
+    const image = `<li class="gallery__item">
+        <a class="gallery__link" href="${galleryItems[i].original}">
+            <img
+                class="gallery__image"
+                src="${galleryItems[i].preview}"
+                alt="${galleryItems[i].description}"
+            />
+        </a>
+    </li>`;
+    images.push(image);
+  }
+  galleryList.innerHTML = images.join("");
+}
